99253b1d1f95155c8e87a36e4c8f0cfa461dde48
SantiagoGaonaC <santiago.gaona.2018@upb.edu.co>
2023-12-19 21:53:37 -0500

    feat: github post create with svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -59,5 +59,15 @@
 				>
 			</a>
 		</li>
+		<li>
+			<a class="group flex items-center py-3" href="#contenido"
+				><span
+					class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
+				></span><span
+					class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"
+					>Contenido</span
+				>
+			</a>
+		</li>
 	</ul>
 </nav>
