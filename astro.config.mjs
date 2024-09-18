79b8766846463ea74b98061a61671fa96bdf1e7d
SantiagoGaonaC <sgaonacarvajal@outlook.com>
2024-09-17 23:19:38 -0500

    feat: change partytown to vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte"
-import vercel from '@astrojs/vercel/serverless';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+
+  output: "server",
+  adapter: vercel()
 });
