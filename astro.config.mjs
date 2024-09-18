8eb0ee73b4e2adea696d2082408e0f69f61dceeb
SantiagoGaonaC <sgaonacarvajal@outlook.com>
2024-09-17 22:47:33 -0500

    feat: partytown added in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
+import svelte from "@astrojs/svelte"
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), partytown({
+     // Adds dataLayer.push as a forwarding-event.
+     config: {
+      forward: ["dataLayer.push"],
+    },
+  })]
 });
